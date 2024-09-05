e494aaad48aa4252ca9b38312cf398e4a34d0813
jwyeager <158490765+jwyeager@users.noreply.github.com>
2024-09-05 14:31:59 -0500

    HW02

--- Homework/HW02_scratch.R
+++ Homework/HW02_scratch.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(rmarkdown)
 library(magrittr)
-
+library(gtools)
 
 ## Section 2.1 Q1
 p.fail <- 0.12
@@ -29,3 +29,23 @@ q4.T <- 1/16
 pr.one.T <- q1.T + q2.T + q3.T + q4.T
 pr.one.T
 #d
+
+
+## Section 2.1 Q5
+#a
+library(gtools)
+candidates <- c(1, 2, 3, 4)
+all.perms <- permutations(n = length(candidates), r = length(candidates), v = candidates)
+colnames(all.perms) <- c('first','second','third','fourth')
+all.perms %<>% as.tibble()
+all.perms$sequence <- paste0("Seq.", 1:24)
+all.perms
+#b
+A <- all.perms %>% filter(first %in% c(1,2))
+A
+#c
+B <- all.perms %>% filter(!first %in% c(1,2)) %>% filter(!second %in% c(1,2))
+B
+#d
+C <- all.perms %>% filter(first %in% c(3,4)) %>% filter(!(first == 4 & second != 3))
+C
