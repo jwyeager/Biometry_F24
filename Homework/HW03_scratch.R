23106568e69df943f2597c02a8b3c742736a934d
jwyeager <158490765+jwyeager@users.noreply.github.com>
2024-09-30 09:51:45 -0500

    3 added

---
+++ Homework/HW03_scratch.R
@@ -0,0 +1,10 @@
+### Biometry HW03 Scratch
+
+# HW03 Commonly Used Distributions
+# Exercises for Section 4.1: 1, 3
+# Exercises for Section 4.2: 1, 5, 7
+# Exercises for Section 4.3: 1, 3, 5
+# Exercises for Section 4.5: 1, 3, 5, 7, 11, 15, 21
+# Exercises for Section 4.9: 1
+# Exercises for Section 4.10 1, 3 (make a QQ plot to demonstrate/support your answer
+#                                  
